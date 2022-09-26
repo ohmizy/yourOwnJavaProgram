c307f108a81314b216e2d36dff7912297a6bd3fb
ohmizy <omarjabr49@yahoo.com>
2022-09-26 14:20:58 -0700

    Initial commit

---
+++ src/HelloCodingDojo.java
@@ -0,0 +1,6 @@
+public class HelloCodingDojo {
+    public static void main(String[]args) {
+        System.out.println("My name is Coding Dojo I am 100 years old My hometown is Burbank, CA");
+//        whatever you want to print goes in these brackets
+    }
+}
